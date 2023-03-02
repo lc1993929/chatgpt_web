13e8e71cd88259428f612ed2d10765c9dee3838d
刘畅 <736779328@qq.com>
2023-03-03 01:07:34 +0800

    更新样式

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
             <body>
             <Header as='h2' icon textAlign='center'>
                 <Image src={logo} className="App-logo" alt="logo"/>
-                <Header.Content>如果长时间未回复或回复了许多重复文本，则为ChatGPT计算错误。请调整你的输入文本</Header.Content>
+                <Header.Content>如果长时间未回复或回复了许多重复文本，则为ChatGPT计算错误。请调整你的输入文本。最大输入文本为193字节</Header.Content>
             </Header>
             <ChatRoom messages={[]}/>
             </body>
